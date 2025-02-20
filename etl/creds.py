68e510b4425c606a520e163ae9025950663f980e
hunterhollis <hunterhollismobile@gmail.com>
2025-02-20 16:57:13 -0600

    Clean-Up

--- etl/creds.py
+++ etl/creds.py
@@ -1,3 +1,3 @@
-api_key = '4be855ecdfc637b8a4a1b96b6cf34b49'
-postgresql_pw = 'postgres'
-geoapify_key = '03d2078ffc4f4b56a8701604bd2aa575'
+# api_key = '[api-sports API key]'
+# postgresql_pw = '[PostgreSQL database password]'
+# geoapify_key = '[geoapify API key]'
